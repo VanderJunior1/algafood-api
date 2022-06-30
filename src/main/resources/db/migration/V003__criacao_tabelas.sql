ca5e917068c3863faa5cb5c579239301dc095135
VanderJunior1 <vanderjunior1@hotmail.com>
2022-06-30 10:52:36 -0300

    Adicionando script criacao tabelas Pedido e ItemPedido

---
+++ src/main/resources/db/migration/V003__criacao_tabelas.sql
@@ -0,0 +1,8 @@
+create table item_pedido (id bigint not null auto_increment, observacao varchar(255), preco_total decimal(19,2), preco_unitario decimal(19,2), quantidade integer, pedido_id bigint not null, produto_id bigint not null, primary key (id)) engine=InnoDB;
+create table pedido (id bigint not null auto_increment, data_cancelamento datetime(6), data_confirmacao datetime(6), data_criacao datetime(6), data_entrega datetime(6), endereco_bairro varchar(20) not null, endereco_cep varchar(9) not null, endereco_complemento varchar(20), endereco_logradouro varchar(20) not null, endereco_numero varchar(10) not null, status integer, subtotal decimal(19,2), taxa_frete decimal(19,2), valor_total decimal(19,2), usuario_cliente_id bigint not null, endereco_cidade_id bigint not null, forma_pagamento_id bigint not null, restaurante_id bigint not null, primary key (id)) engine=InnoDB;
+alter table item_pedido add constraint FK60ym08cfoysa17wrn1swyiuda foreign key (pedido_id) references pedido (id);
+alter table item_pedido add constraint FKtk55mn6d6bvl5h0no5uagi3sf foreign key (produto_id) references produto (id);
+alter table pedido add constraint FKcccmjvm9ytuxbe00h3wmtm77y foreign key (usuario_cliente_id) references usuario (id);
+alter table pedido add constraint FKk987vfg9cpgx7qxj3166fdqig foreign key (endereco_cidade_id) references cidade (id);
+alter table pedido add constraint FKqaa411xsl0xu4tkvt1wpccd3b foreign key (forma_pagamento_id) references forma_pagamento (id);
+alter table pedido add constraint FK3eud5cqmgsnltyk704hu3qj71 foreign key (restaurante_id) references restaurante (id);
