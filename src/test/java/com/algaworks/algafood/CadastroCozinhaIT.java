3c4dd1c91f1e0adaef7a7cc0e8c9bd420413a46e
VanderJunior1 <vanderjunior1@hotmail.com>
2022-07-03 16:05:43 -0300

    Adicionando o arquivo afterMigrate.sql, alterando a classe de teste para usar um bd especifico

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,14 +1,16 @@
 package com.algaworks.algafood;
 
-import org.hamcrest.Matcher;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import io.restassured.RestAssured;
@@ -16,16 +18,22 @@ import io.restassured.http.ContentType;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private Flyway flyway;
 	
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 	}
 
 	@Test
@@ -39,7 +47,7 @@ public class CadastroCozinhaIT {
 	}
 	
 	@Test
-	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+	public void deveConterMaiorOuIgualQuatroCozinhas_QuandoConsultarCozinhas() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		
 		RestAssured.given()
@@ -47,7 +55,18 @@ public class CadastroCozinhaIT {
 		.when()
 			.get()
 		.then()
-			.body("nome", Matchers.hasSize(13))
+			.body("nome", Matchers.hasSize(4))
 			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinhas() {
+		RestAssured.given()
+			.body("{\"nome\": \"Chinesa\"}") 
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
 	}
 }
