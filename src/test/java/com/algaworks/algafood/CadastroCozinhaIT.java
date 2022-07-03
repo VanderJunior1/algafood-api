849426c841bda2221d4ff0d63e18b29e37a13f98
VanderJunior1 <vanderjunior1@hotmail.com>
2022-07-03 16:19:30 -0300

    Limpando e populando o banco de dados de teste

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood;
 
-import org.flywaydb.core.Flyway;
+import java.util.Arrays;
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,10 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.algaworks.algafood.domain.Cozinha;
+import com.algaworks.algafood.repository.CozinhaRepository;
+import com.algaworks.algafood.util.DatabaseCleaner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -25,7 +30,10 @@ public class CadastroCozinhaIT {
 	private int port;
 	
 	@Autowired
-	private Flyway flyway;
+	CozinhaRepository cozinhaRepository;
+	
+	@Autowired
+	private DatabaseCleaner databaseCleaner;
 	
 	@BeforeEach
 	public void setUp() {
@@ -33,7 +41,8 @@ public class CadastroCozinhaIT {
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
 		
-		flyway.migrate();
+		databaseCleaner.clearTables();
+		prepararDados();
 	}
 
 	@Test
@@ -47,7 +56,7 @@ public class CadastroCozinhaIT {
 	}
 	
 	@Test
-	public void deveConterMaiorOuIgualQuatroCozinhas_QuandoConsultarCozinhas() {
+	public void deveConterDuasCozinhas_QuandoConsultarCozinhas() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		
 		RestAssured.given()
@@ -55,7 +64,7 @@ public class CadastroCozinhaIT {
 		.when()
 			.get()
 		.then()
-			.body("nome", Matchers.hasSize(4))
+			.body("nome", Matchers.hasSize(2))
 			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
 	}
 	
@@ -68,5 +77,13 @@ public class CadastroCozinhaIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	private void prepararDados() {
+		var cozinha1 = new Cozinha();
+		cozinha1.setNome("Indiana");
+		var cozinha2 = new Cozinha();
+		cozinha2.setNome("Tailandesa");
+		cozinhaRepository.saveAll(Arrays.asList(cozinha1, cozinha2));
 	}
 }
