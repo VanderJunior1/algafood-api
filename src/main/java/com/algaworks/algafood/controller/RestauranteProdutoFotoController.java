244655fd4903631ae2d212bdcc57c5db84c511c3
VanderJunior1 <vanderjunior1@hotmail.com>
2022-07-07 17:28:29 -0300

    Implementando upload de arquivo e validando o tamanho maximo do arquivo

--- src/main/java/com/algaworks/algafood/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/controller/RestauranteProdutoFotoController.java
@@ -3,13 +3,15 @@ package com.algaworks.algafood.controller;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.dto.input.FotoProdutoInput;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,24 +19,27 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
+	
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
-			@RequestParam MultipartFile arquivo) {
-		log.info("Upload de imagem com nome {}", arquivo.getOriginalFilename());
-
-		var nomeArquivo = UUID.randomUUID().toString() + "_" + arquivo.getOriginalFilename();
-		var arquivoFoto = Path.of("C:\\upload", nomeArquivo);
+	public void atualizarFoto(@PathVariable Long restauranteId,
+			@PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+		log.info("Upload de imagem com nome {}", fotoProdutoInput.getArquivo().getOriginalFilename());
+		var nomeArquivo = UUID.randomUUID().toString() 
+				+ "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+		
+		var arquivoFoto = Path.of("C:\\fotos", nomeArquivo);
+		
+		System.out.println(fotoProdutoInput.getDescricao());
 		System.out.println(arquivoFoto);
-		System.out.println(arquivo.getContentType());
-		System.out.println(arquivo.getOriginalFilename());
-
+		System.out.println(fotoProdutoInput.getArquivo().getContentType());
+		
 		try {
-			arquivo.transferTo(arquivoFoto);
+			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-
+		
 	}
 
 }
---
+++ src/main/java/com/algaworks/algafood/dto/input/FotoProdutoInput.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.dto.input;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.validator.FileContentType;
+import com.algaworks.algafood.validator.FileSize;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class FotoProdutoInput {
+	
+	@NotNull
+	@FileSize(max = "500KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
+	private MultipartFile arquivo;
+	
+	@NotBlank
+	private String descricao;
+}
