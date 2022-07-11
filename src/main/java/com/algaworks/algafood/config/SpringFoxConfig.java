ec94529ed888484a9c803b97c9d753994ea01f54
VanderJunior1 <vanderjunior1@hotmail.com>
2022-07-11 16:41:18 -0300

    Referenciando modelo de representação de problema com códigos de status de erro

--- src/main/java/com/algaworks/algafood/config/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/config/SpringFoxConfig.java
@@ -1,16 +1,27 @@
 package com.algaworks.algafood.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.algaworks.algafood.exceptionhandler.ApiError;
+import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -23,16 +34,79 @@ public class SpringFoxConfig implements WebMvcConfigurer  {
 
 	@Bean
 	public Docket apiDocket() {
+		TypeResolver typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.controller"))
 					.build()
+					.useDefaultResponseMessages(false)
+					.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
+					.globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
+					.globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
+					.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
+					.additionalModels(typeResolver.resolve(ApiError.class))	
 				.apiInfo(apiInfo())
 				.tags(
 						new Tag("Cidades", "Gerencia as cidades")
 					 	  );
 	}
 	
+	private List<ResponseMessage> globalDeleteResponseMessages() {
+		return Arrays.asList(
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.BAD_REQUEST.value())
+	                .message("Requisição inválida (erro do cliente)")
+	                .responseModel(new ModelRef("Api Erros"))
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+	                .message("Erro interno no servidor")
+	                .responseModel(new ModelRef("Api Erros"))
+	                .build()
+	        );
+	}
+
+	private List<ResponseMessage> globalPostPutResponseMessages() {
+		return Arrays.asList(
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.BAD_REQUEST.value())
+	                .message("Requisição inválida (erro do cliente)")
+	                .responseModel(new ModelRef("Api Erros"))
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+	                .message("Erro interno no servidor")
+	                .responseModel(new ModelRef("Api Erros"))
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.NOT_ACCEPTABLE.value())
+	                .message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
+	                .message("Requisição recusada porque o corpo está em um formato não suportado")
+	                .responseModel(new ModelRef("Api Erros"))
+	                .build()
+	        );
+	}
+
+	private List<ResponseMessage> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+						.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+						.message("Erro interno do Servidor")
+						.responseModel(new ModelRef("Api Erros"))
+						.responseModel(new ModelRef("Api Erros"))
+						.build(),
+				new ResponseMessageBuilder()
+						.code(HttpStatus.NOT_ACCEPTABLE.value())
+						.message("Recurso não possui representação que pode ser aceita pelo consumidor")
+						.build()
+		);
+	}
+	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 	    registry.addResourceHandler("swagger-ui.html")
--- src/main/java/com/algaworks/algafood/exceptionhandler/ApiError.java
+++ src/main/java/com/algaworks/algafood/exceptionhandler/ApiError.java
@@ -6,27 +6,46 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+@ApiModel("Api Erros")
 @JsonInclude(Include.NON_NULL)
-@Builder @Getter 
+@Builder @Getter
 public class ApiError {
 
+	@ApiModelProperty(example = "400", position = 1)
 	private Integer status;
+
+	@ApiModelProperty(example = "https://algafood.com/erro-negocio", position = 10)
 	private String type;
+
+	@ApiModelProperty(example = "Violação de regra de negócio", position = 20)
 	private String title;
+
+	@ApiModelProperty(example = "Não existe um cadastro de estado com codigo 1", position = 25)
 	private String detail;
-	
+
+	@ApiModelProperty(example = "Não existe um cadastro de estado com codigo 1", position = 30)
 	private String userMessage;
+
+	@ApiModelProperty(example = "2022-07-11T19:23:50.344+00:00", position = 35)
 	private OffsetDateTime timestamp;
+
+	@ApiModelProperty(value = "Objetos ou campos que geraram o erro", position = 40)
 	private List<Object> objects;
-	
+
+	@ApiModel("Objeto Problema")
 	@Getter
 	@Builder
 	public static class Object {
 
+		@ApiModelProperty(example = "Estado")
 		private String name;
+
+		@ApiModelProperty(example = "Estado não encontrado")
 		private String userMessage;
 	}
 
